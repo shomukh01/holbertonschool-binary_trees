377db009cedc806bf8eaec65d187104748250e8e
Shomukh01 <Shomukh.aldosari@gmail.com>
2026-05-06 08:39:53 +0000

    Add BST remove function

---
+++ 114-bst_remove.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * bst_min - Finds the minimum node in a BST
+ * @tree: Pointer to the root node
+ *
+ * Return: Pointer to the minimum node
+ */
+bst_t *bst_min(bst_t *tree)
+{
+	while (tree && tree->left)
+		tree = tree->left;
+
+	return (tree);
+}
+
+/**
+ * bst_remove - Removes a node from a BST
+ * @root: Pointer to the root node
+ * @value: Value to remove
+ *
+ * Return: Pointer to the new root node
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *tmp;
+
+	if (root == NULL)
+		return (NULL);
+
+	if (value < root->n)
+	{
+		root->left = bst_remove(root->left, value);
+		if (root->left)
+			root->left->parent = root;
+	}
+	else if (value > root->n)
+	{
+		root->right = bst_remove(root->right, value);
+		if (root->right)
+			root->right->parent = root;
+	}
+	else
+	{
+		if (root->left == NULL)
+		{
+			tmp = root->right;
+			if (tmp)
+				tmp->parent = root->parent;
+			free(root);
+			return (tmp);
+		}
+		if (root->right == NULL)
+		{
+			tmp = root->left;
+			if (tmp)
+				tmp->parent = root->parent;
+			free(root);
+			return (tmp);
+		}
+
+		tmp = bst_min(root->right);
+		root->n = tmp->n;
+		root->right = bst_remove(root->right, tmp->n);
+		if (root->right)
+			root->right->parent = root;
+	}
+
+	return (root);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -54,8 +54,6 @@ int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
-<<<<<<< HEAD
+bst_t *bst_remove(bst_t *root, int value);
 
-=======
->>>>>>> d169844 (Update binary trees tasks)
 #endif
