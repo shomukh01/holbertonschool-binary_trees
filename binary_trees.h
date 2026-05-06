427377077ac25ddf850d61a2f9ef328101f2712a
laila-khalid <13252@holbertonstudents.com>
2026-05-06 10:25:00 +0000

    Add prototype for bst_min_val and fix task 29

--- binary_trees.h
+++ binary_trees.h
@@ -55,6 +55,7 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
+bst_t *bst_min_val(bst_t *root);
 <<<<<<< HEAD
 
 =======
