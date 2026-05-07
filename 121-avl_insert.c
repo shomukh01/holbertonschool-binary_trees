10e370d02fa0795260f2c28e87c1f67ce0db25d0
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-05-07 05:22:59 +0000

    Fix AVL insert logic and root update

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -18,33 +18,27 @@ avl_t *r_insert_node(avl_t **tree, avl_t *parent,
 		return (*new_node = binary_tree_node(parent, value));
 
 	if (value < (*tree)->n)
-	{
-		(*tree)->left = r_insert_node(&((*tree)->left), *tree,
-					      new_node, value);
-	}
+		(*tree)->left = r_insert_node(&((*tree)->left), *tree, new_node, value);
 	else if (value > (*tree)->n)
-	{
-		(*tree)->right = r_insert_node(&((*tree)->right), *tree,
-					       new_node, value);
-	}
+		(*tree)->right = r_insert_node(&((*tree)->right), *tree, new_node, value);
 	else
 		return (*tree);
 
 	b_factor = binary_tree_balance(*tree);
 
-	if (b_factor > 1 && value < (*tree)->left->n)
+	if (b_factor > 1 && value < (*tree)->left->n) /* LL Case */
 		return (binary_tree_rotate_right(*tree));
 
-	if (b_factor < -1 && value > (*tree)->right->n)
+	if (b_factor < -1 && value > (*tree)->right->n) /* RR Case */
 		return (binary_tree_rotate_left(*tree));
 
-	if (b_factor > 1 && value > (*tree)->left->n)
+	if (b_factor > 1 && value > (*tree)->left->n) /* LR Case */
 	{
 		(*tree)->left = binary_tree_rotate_left((*tree)->left);
 		return (binary_tree_rotate_right(*tree));
 	}
 
-	if (b_factor < -1 && value < (*tree)->right->n)
+	if (b_factor < -1 && value < (*tree)->right->n) /* RL Case */
 	{
 		(*tree)->right = binary_tree_rotate_right((*tree)->right);
 		return (binary_tree_rotate_left(*tree));
@@ -73,7 +67,8 @@ avl_t *avl_insert(avl_t **tree, int value)
 		return (*tree);
 	}
 
-	r_insert_node(tree, NULL, &new_node, value);
+	/* التعديل الجوهري: لازم نساوي الجذر بنتيجة الدالة */
+	*tree = r_insert_node(tree, NULL, &new_node, value);
 
 	return (new_node);
 }
