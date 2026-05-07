ff7274678f3a19181bfc6ea887c5165fea9ce41e
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-05-07 04:54:10 +0000

    Organize binary_trees.h for better readability and clean code

--- binary_trees.h
+++ binary_trees.h
@@ -2,7 +2,9 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -25,7 +27,10 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* Function prototypes */
+/* Helper function */
+void binary_tree_print(const binary_tree_t *tree);
+
+/* Mandatory functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -45,6 +50,8 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/* Advanced functions */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
@@ -57,4 +64,4 @@ bst_t *bst_search(const bst_t *tree, int value);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
 
-#endif
+#endif /* BINARY_TREES_H */
